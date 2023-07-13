b9b8e73ae93c76fccc920f2a4cb2a0e4e4df1ad8
Christopher Gabriel Ozuna Batista <srsteve1913@gmail.com>
2023-07-13 09:49:37 -0400

    Cambios a controlador

--- controllers/editCompanieController.js
+++ controllers/editCompanieController.js
@@ -13,30 +13,29 @@ exports.getCompaniesEdit = async (req, res) => {
   }
 };
 
-  exports.formPut = async (req, res) => {
-    try {
-      const companyId = req.params.id;
-      const newData = req.body;
-      const options = { new: true };
-  
-      let currentDate = new Date(newData.dateClose);
-  
-      currentDate.setDate(currentDate.getDate() + 1);
-      newData.dateClose = currentDate;
-  
-      const updatedCompany = await companyModel.findByIdAndUpdate(
-        companyId,
-        newData,
-        options
-      );
-  
-      if (updatedCompany) {
-        return res.redirect("/dashboard");
-      } else {
-        res.json({ success: false });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).send("No se pudo actualizar la data");
+exports.formPut = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const options = { new: true };
+
+    let currentDate = new Date(req.body.dateClose);
+
+    currentDate.setDate(currentDate.getDate() + 1);
+    req.body.dateClose = currentDate;
+
+    const updatedCompany = await companyModel.findByIdAndUpdate(
+      companyId,
+      req.body,
+      options
+    );
+
+    if (updatedCompany) {
+      return res.redirect("/dashboard");
+    } else {
+      res.json({ success: false });
     }
-  };
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("No se pudo actualizar la data");
+  }
+};
